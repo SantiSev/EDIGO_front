3724481f6aaf193b8de84916fc2d501512402ddc
SantiSev <santisevitz@hotmail.com>
2024-03-02 17:00:44 -0300

    articleThumbnail requires finishinggg

--- src/components/ArticleThumbnail.tsx
+++ src/components/ArticleThumbnail.tsx
@@ -10,10 +10,14 @@ interface ArticleThumbnailProps_ {
 
 const ArticleThumbnail: React.FC<ArticleThumbnailProps_> = ({ article }) => {
     return (
-        <div className="-mx-16 my-2 flex flex-col items-center ">
-            <div className='relative hover:scale-110 transition-all rounded-md w-1/2 h-1/2 cursor-pointer'>
-                <img src={article.imageUrl} alt={article.name} className="object-cover" />
-                <p className="absolute mt-2 text-center items-center">{article.name}</p>
+        <div className="-mx-16 my-2 flex flex-col items-center">
+            <div className='relative hover:scale-110 transition-all rounded-md w-1/2 h-1/2 cursor-pointer overflow-hidden'>
+                <div className="w-full h-full">
+                    <img src={article.imageUrl} alt={article.name} className="object-cover w-full h-full" />
+                </div>
+                <div className="absolute inset-0 flex flex-col justify-center items-center">
+                    <p className="mt-2 text-center">{article.name}</p>
+                </div>
             </div>
         </div>
     )
